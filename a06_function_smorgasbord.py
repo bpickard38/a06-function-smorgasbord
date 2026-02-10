019a24a7c04524f53787f7b8a8749ce5ddf39a0e
bpickard38 <bpickard38@gmail.com>
2026-02-10 10:13:33 -0700

    finished

--- a06_function_smorgasbord.py
+++ a06_function_smorgasbord.py
@@ -1,3 +1,41 @@
-# Name:
+# Name: Brayden Pickard
 # Enter your python code below. In this file, you will call all of the
 # functions that you defined in your my_functions.py file.
+
+import a06_my_functions
+
+def welcome_message(name):
+    print(f"Hello {name}, welcome to IS 303!")
+
+welcome_message("Diego")
+welcome_message("Mai")
+
+print(a06_my_functions.sum_two_numbers(5, 7))
+print(a06_my_functions.sum_two_numbers(1000.5, -30))
+
+print(a06_my_functions.is_even(7))
+print(a06_my_functions.is_even(120))
+
+print(a06_my_functions.get_number_parity(5))
+print(a06_my_functions.get_number_parity(10))
+
+print(a06_my_functions.fahrenheit_to_celsius(32))
+print(a06_my_functions.fahrenheit_to_celsius(75))
+
+#calls these functions
+numbers_list_1 = [20, 45, 23, 2, 87, 3]
+print(a06_my_functions.min_max_mean(numbers_list_1))
+
+print(a06_my_functions.dog_message("Spot", 7))
+print(a06_my_functions.dog_message("Peppy"))
+print(a06_my_functions.classify_age(60, 55))
+print(a06_my_functions.classify_age(62))
+
+for _ in range(2):
+    price = float(input("Enter the price for the product purchased: "))
+    quantity = int(input("Enter the quantity of the product purchased: "))
+    discount_percent = float(input("Enter the discount percent (formatted as a decimal): "))
+    total2 = a06_my_functions.calculate_total(price, quantity, discount_percent)
+    print(f"The total price after discounts is: ${total2}")
+
+#this are the inputs
--- a06_my_functions.py
+++ a06_my_functions.py
@@ -1,2 +1,46 @@
-# Name:
-# You will define most of your functions for this assignment in this file.
+# Name: Brayden Pickard
+# These are all the functions
+
+def sum_two_numbers(a, b):
+    sum = a + b
+    return sum
+#this sums numbers
+def is_even(num):
+    result = num % 2 == 0
+    return result
+    
+def get_number_parity(num):
+    if is_even(num) == True:
+        return f"{num} is even"
+    else:
+        return f"{num} is odd"
+
+def fahrenheit_to_celsius(fahrenheit):
+    C = (fahrenheit - 32) * (5/9)
+    return C
+#this converts fahrenheit to celsius
+def min_max_mean(numbers_list):
+    min_num = min(numbers_list)
+    max_num = max(numbers_list)
+    mean = sum(numbers_list) / len(numbers_list)
+    return [min_num, max_num, mean]
+
+def dog_message(name, age=0):
+    return f"I am a dog named {name} and I'm {age} years old!"
+
+def classify_age(age, senior_age=65):
+    if age <18:
+        return "Minor"
+    elif age < senior_age:
+        return "Adult"
+    else:
+        return "Senior"
+#dog messages
+def calculate_total(price, quantity, discount_percent= .1, threshold_total=100, bonus_discount=.02):
+    total_price = price * quantity
+    if total_price <= threshold_total:
+        grand_total = total_price * (1- discount_percent)
+        return round(grand_total, 2)
+    elif total_price > threshold_total:
+        grand_total = total_price * (1-(discount_percent+ bonus_discount))
+        return round(grand_total, 2)
